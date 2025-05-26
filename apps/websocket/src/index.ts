8680942caabcc9ca5a5226c209db41646ab8dc8d
Bibek Tamang <bibek7here@gmail.com>
2025-05-26 14:26:41 +0545

    handle socket event

--- apps/websocket/src/index.ts
+++ apps/websocket/src/index.ts
@@ -1,22 +1,153 @@
-import { User } from "./user";
+import { User, userManager, type TUser } from "./user";
 import type { WebSocketHandler } from "bun";
-import { getUserFromToken } from "../../web/helpers/verifyToken";
-// import { redisClient } from "redis-config";
+import { getUserFromToken } from "./verifyToken";
+import { chatManager } from "./chat";
+
+import {
+	ACCEPT_JOIN_CALL,
+	DELETE_MESSAGE_FROM_CHAT,
+	LEAVE_CALL,
+	MOVEMENT,
+	RECEIVE_MESSAGE_IN_CHAT,
+	REJECT_JOIN_CALL,
+	REMOVE_USER_FROM_CHAT,
+	REMOVED_FROM_CHAT,
+	REQUEST_CHAT_MESSAGE,
+	REQUEST_JOIN_CALL,
+	SEND_MESSAGE_IN_CHAT,
+} from "./constants";
 
 export type SocketData = {
-	user: {
-		id: string;
-		image: string;
-	};
+	user: TUser;
+	location: { longitude: number; latitude: number };
+};
+
+const handleMessage = (
+	ws: Bun.ServerWebSocket<SocketData>,
+	message: string | Buffer<ArrayBufferLike>
+) => {
+	const parsedMessage = JSON.parse(String(message));
+	const payload = parsedMessage.payload;
+	switch (parsedMessage.type) {
+		case MOVEMENT:
+			const socketUser = userManager.getUser(ws.data.user.id);
+			if (socketUser) {
+				socketUser.updateLocation(payload);
+			}
+			break;
+		case REQUEST_CHAT_MESSAGE:
+			const creatorSocket = userManager.getUser(payload.requestBy);
+			const requestedSocket = userManager.getUser(payload.requestTo);
+			if (creatorSocket && requestedSocket) {
+				const userId = creatorSocket.getUserId();
+				const chat = chatManager.createChat(userId, creatorSocket);
+				chat.addParticipant(requestedSocket);
+				requestedSocket.getSocket().send(
+					JSON.stringify({
+						type: REQUEST_CHAT_MESSAGE,
+						payload: {
+							requestBy: {
+								id: creatorSocket.getUserId(),
+								location: creatorSocket.getLocation(),
+								image: creatorSocket.getImage(),
+								interests: creatorSocket.getInterests(),
+							},
+						},
+					})
+				);
+			}
+			break;
+		case SEND_MESSAGE_IN_CHAT: {
+			const senderUser = userManager.getUser(payload.sender);
+			const chat = chatManager.getChat(payload.chatId);
+			if (senderUser && chat) {
+				const senderUserId = senderUser.getUserId();
+				chat.broadcastMessage(
+					JSON.stringify({
+						type: RECEIVE_MESSAGE_IN_CHAT,
+						payload: {
+							messageId: senderUserId,
+							content: payload.content,
+							sender: senderUserId,
+						},
+					}),
+					senderUserId
+				);
+			}
+			break;
+		}
+		case DELETE_MESSAGE_FROM_CHAT: {
+			const senderUser = userManager.getUser(payload.sender);
+			const chat = chatManager.getChat(payload.chatId);
+			if (chat && senderUser) {
+				const senderUserId = senderUser.getUserId();
+				chat.broadcastMessage(
+					JSON.stringify({
+						type: DELETE_MESSAGE_FROM_CHAT,
+						payload: {
+							messageId: payload.messageId,
+						},
+					}),
+					senderUserId
+				);
+			}
+			break;
+		}
+		case REMOVE_USER_FROM_CHAT:
+			const senderUser = userManager.getUser(payload.sender);
+			const chat = chatManager.getChat(payload.chatId);
+			if (senderUser && chat) {
+				const senderId = senderUser.getUserId();
+				if (senderId === chat.chatId) {
+					const requestUser = userManager.getUser(payload.requestRemoveUserId);
+					chatManager.removeUserFromChat(requestUser!);
+					requestUser?.getSocket().send(
+						JSON.stringify({
+							type: REMOVED_FROM_CHAT,
+							payload: {
+								chatId: chat.chatId,
+							},
+						})
+					);
+					// TODO: REMOVE USER IF IN MEDIASOUP
+				}
+			}
+			break;
+		case LEAVE_CALL:
+			break;
+		case REQUEST_JOIN_CALL:
+			break;
+		case ACCEPT_JOIN_CALL:
+			break;
+		case REJECT_JOIN_CALL:
+			break;
+	}
 };
 
 const websocketHandle: WebSocketHandler<SocketData> = {
 	open(ws) {
-		// const socketUser = new User(ws.data.user.id, ws);
-		ws.send("hello from websocket")
+		const socketUser = new User(
+			ws.data.user.id,
+			ws,
+			ws.data.user.image,
+			ws.data.location,
+			ws.data.user.interests
+		);
+		userManager.addUser(socketUser);
+		console.log("Socket user created.");
 	},
-	message(ws, message) {},
-	close() {},
+	message(ws, message) {
+		handleMessage(ws, message);
+	},
+	close(ws, code, reason) {
+		const socketUser = userManager.getUser(ws.data.user.id);
+		if (socketUser) {
+			userManager.removeUser(socketUser);
+			chatManager.removeUserFromChat(socketUser);
+		}
+		console.log("socket close code: ", code);
+		console.log("socket close reason", reason);
+	},
 	perMessageDeflate: true,
 };
 
@@ -33,26 +164,24 @@ function parseCookie(cookieHeader: string, name: string) {
 const server = Bun.serve({
 	port: 8080,
 	async fetch(req, server) {
-		// const cookie = req.headers.get("cookie");
-		// if (!cookie) return new Response("Unauthenticated");
-		// const token =
-		// 	parseCookie(cookie, "__Secure-next-auth.session-token") ??
-		// 	parseCookie(cookie, "next-auth.session-token");
-
-		// if (!token) {
-		// 	return new Response("Unauthenticated");
-		// }
-		// const user = await getUserFromToken(token);
-		// if (!user) {
-		// 	return new Response("Invalid token");
-		// }
+		const url = new URL(req.url);
+		const token = url.searchParams.get("token");
+		const location = url.searchParams.get("location");
+		if (!token) {
+			return new Response("Unathenticated");
+		}
+		const user = getUserFromToken(token);
+		if (!user) return new Response("Invalid token", { status: 403 });
 		const success = server.upgrade(req, {
-			data: {},
+			data: {
+				user,
+				location,
+			},
 		});
 		if (success) {
 			return undefined;
 		}
-		return new Response("Hello world");
+		return new Response("Failed to upgrage server");
 	},
 	websocket: websocketHandle,
 });
