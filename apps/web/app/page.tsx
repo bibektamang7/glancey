8696a8a79992ed6960c42636552880bd9651fc2b
Bibek Tamang <bibek7here@gmail.com>
2025-06-02 15:09:35 +0545

    landing page init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,27 +1,35 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import Hero from "@/pages/Hero/Hero";
 
 type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
+	srcLight: string;
+	srcDark: string;
 };
 
 const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
+	const { srcLight, srcDark, ...rest } = props;
 
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
+	return (
+		<>
+			<Image
+				{...rest}
+				src={srcLight}
+				className="imgLight"
+			/>
+			<Image
+				{...rest}
+				src={srcDark}
+				className="imgDark"
+			/>
+		</>
+	);
 };
 
 export default function Home() {
-  return (
-    <div className={`text-green-200`}>
-      <p>Hello world</p> 
-    </div>
-  );
+	return (
+		<div className="!h-full">
+			<Hero />
+		</div>
+	);
 }
