9bdbb37924220d23f3e1ed86fe9767a571489c44
Bibek Tamang <bibek7here@gmail.com>
2025-06-08 22:52:59 +0545

    added params

--- apps/web/hooks/useChat.tsx
+++ apps/web/hooks/useChat.tsx
@@ -15,6 +15,8 @@ import {
 	REQUEST_CHAT_MESSAGE,
 	REQUEST_JOIN_CALL,
 } from "socket-events";
+import { User } from "@/types/user";
+import { toast } from "sonner";
 
 export const useChat = () => {
 	const [chats, setChats] = useState<Chat[]>([]);
@@ -39,31 +41,43 @@ export const useChat = () => {
 		}
 	};
 
-	// Socket events function
-	const handleRequestChatMessage = () => {};
-	const handleMessageReceive = () => {};
-	const handleMessageDelete = () => {};
-	const hanldeRemoveUserFromChat = () => {};
-
-	// media soup events
-	const handleUserLeaveCall = () => {};
-	const handleIncomingCall = () => {};
-	const handleRequestJoinCall = () => {};
-	const handleIncomingCallAccepted = () => {};
-	const handleIncomingCallRejected = () => {};
-	const handleIncomingCallJoinRequest = () => {};
-	const handleIncomingCallJoinAccepted = () => {};
-	const handleRejectIncomingCallJoinRequest = () => {};
-
-	const handleRtpCapabilities = () => {};
-	const handleProducerTransportCreated = () => {};
-	const handeProducerConnected = () => {};
-	const handleProducedMedia = () => {};
-	const handleConsumerTransportCreated = () => {};
-	const handleConsumerTransportConnected = () => {};
-	const handleSubscribed = () => {};
-	const handleResumed = () => {};
-	const handleErrorOnMedia = () => {};
+	const handleRequestChatMessage = (
+		user: User,
+		chatId: string,
+		message: string
+	) => {
+		toast(`New message from ${user.name}`);
+	};
+	const handleMessageReceive = (
+		sender: User,
+		messageId: string,
+		message: string,
+		chatId: string
+	) => {};
+	const handleDeleteMessage = (messageId: string, userId: string) => {};
+	const handleRemoveUserFromChat = (
+		chatId: string,
+		removedUserId: string
+	) => {};
+	const handleLeaveCall = (userId: string) => {};
+	const handleIncomingCall = (chatId: string, requestedUser: User) => {};
+	const handleRequestJoinCall = (requestedBy: User) => {};
+	const handleIncomingCallAccepted = (chatId: string, acceptedUser: User) => {};
+	const handleIncomingCallJoinRequest = (senderUser: User) => {};
+	const handleIncomingCallJoinAccepted = (chatId: string) => {};
+	const handleRejectIncomingJoinRequest = (rejectedBy: User) => {};
+	const handleRtpCapabilities = (chatId: string, rtpCapabilities: any) => {};
+	const handleProducerTransportCreated = (params: any) => {};
+	const handleProducedMedia = (
+		user: User,
+		chatId: string,
+		producerId: string
+	) => {};
+	const handleConsumerTransportCreated = (params: any, chatId: string) => {};
+	const handleConsumerTransportConnected = (message: any, chatId: string) => {};
+	const handleSubscribed = (params: any, chatId: string) => {};
+	const handleResumed = (message: any) => {};
+	const handleErrorOnMediaSoup = (message: string) => {};
 
 	useEffect(() => {
 		messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -74,93 +88,102 @@ export const useChat = () => {
 			socket.onmessage = (event) => {
 				const message = JSON.parse(event.data);
 				const payload = message.payload;
-
 				switch (message.type) {
 					case REQUEST_CHAT_MESSAGE: {
-						handleRequestChatMessage();
+						handleRequestChatMessage(
+							payload.requestBy,
+							payload.chatId,
+							payload.message
+						);
 						break;
 					}
 					case RECEIVE_MESSAGE_IN_CHAT: {
-						handleMessageReceive();
+						handleMessageReceive(
+							payload.sender,
+							payload.messageId,
+							payload.content,
+							payload.chatId
+						);
 						break;
 					}
 					case DELETE_MESSAGE_FROM_CHAT: {
-						handleMessageDelete();
+						handleDeleteMessage(payload.messageId, payload.by);
 						break;
 					}
 					case REMOVED_FROM_CHAT: {
-						hanldeRemoveUserFromChat();
+						handleRemoveUserFromChat(payload.chatId, payload.removedUserId);
 						break;
 					}
 					case LEAVE_CALL: {
-						handleUserLeaveCall();
+						handleLeaveCall(payload.leftUserId);
 						break;
 					}
 					case INCOMING_CALL: {
-						handleIncomingCall();
+						//TODO: need to handle call in the call, already existed chat
+						handleIncomingCall(payload.chatId, payload.requestBy);
 						break;
 					}
 					case REQUEST_JOIN_CALL: {
-						handleRequestJoinCall();
+						handleRequestJoinCall(payload.requestBy);
 						break;
 					}
 					case INCOMING_CALL_ACCEPTED: {
-						handleIncomingCallAccepted();
+						handleIncomingCallAccepted(payload.chatId, payload.acceptedBy);
 						break;
 					}
-
 					case INCOMING_CALL_REJECTED: {
-						handleIncomingCallRejected();
+						// TODO: NOT quite sure
+						// handleIncomingCallRejected(payload.rejectedBy)
 						break;
 					}
 					case INCOMING_CALL_JOIN_REQUEST: {
-						handleIncomingCallJoinRequest();
+						handleIncomingCallJoinRequest(payload.from);
 						break;
 					}
 					case INCOMING_CALL_JOIN_ACCEPTED: {
-						handleIncomingCallJoinAccepted();
+						// TODO: NOT SURE as well
+						handleIncomingCallJoinAccepted(payload.chatId);
 						break;
 					}
 					case REJECT_INCOMING_CALL_JOIN_REQUEST: {
-						handleRejectIncomingCallJoinRequest();
+						handleRejectIncomingJoinRequest(payload.rejectedBy);
 						break;
 					}
-
-					// media soup events
 					case "rtpCapabilities": {
-						handleRtpCapabilities();
+						handleRtpCapabilities(payload.chatId, payload.rtpCapabilities);
 						break;
 					}
 					case "producer_transport_created": {
-						handleProducerTransportCreated();
+						handleProducerTransportCreated(payload);
 						break;
 					}
-					case "producer_connected": {
-						handeProducerConnected();
-						break;
-					}
+					// ONE LEFT HERE
 					case "produced_media": {
-						handleProducedMedia();
+						handleProducedMedia(
+							payload.user,
+							payload.chatId,
+							payload.producerId
+						);
 						break;
 					}
 					case "consumer_transport_created": {
-						handleConsumerTransportCreated();
+						handleConsumerTransportCreated(payload.params, payload.chatId);
 						break;
 					}
 					case "consumer_transport_connected": {
-						handleConsumerTransportConnected();
+						handleConsumerTransportConnected(payload.message, payload.chatId);
 						break;
 					}
 					case "subscribed": {
-						handleSubscribed();
+						handleSubscribed(payload.params, payload.chatId);
 						break;
 					}
 					case "resumed": {
-						handleResumed();
+						handleResumed(payload.message);
 						break;
 					}
 					case "error_on_media": {
-						handleErrorOnMedia();
+						handleErrorOnMediaSoup(payload.message);
 						break;
 					}
 				}
@@ -179,3 +202,119 @@ export const useChat = () => {
 		messagesEndRef,
 	};
 };
+
+// // Socket events function
+// const handleRequestChatMessage = () => {};
+// const handleMessageReceive = () => {};
+// const handleMessageDelete = () => {};
+// const hanldeRemoveUserFromChat = () => {};
+
+// // media soup events
+// const handleUserLeaveCall = () => {};
+// const handleIncomingCall = () => {};
+// const handleRequestJoinCall = () => {};
+// const handleIncomingCallAccepted = () => {};
+// const handleIncomingCallRejected = () => {};
+// const handleIncomingCallJoinRequest = () => {};
+// const handleIncomingCallJoinAccepted = () => {};
+// const handleRejectIncomingCallJoinRequest = () => {};
+
+// const handleRtpCapabilities = () => {};
+// const handleProducerTransportCreated = () => {};
+// const handeProducerConnected = () => {};
+// const handleProducedMedia = () => {};
+// const handleConsumerTransportCreated = () => {};
+// const handleConsumerTransportConnected = () => {};
+// const handleSubscribed = () => {};
+// const handleResumed = () => {};
+// const handleErrorOnMedia = () => {};
+
+// switch (message.type) {
+// 	case REQUEST_CHAT_MESSAGE: {
+// 		handleRequestChatMessage();
+// 		break;
+// 	}
+// 	case RECEIVE_MESSAGE_IN_CHAT: {
+// 		handleMessageReceive();
+// 		break;
+// 	}
+// 	case DELETE_MESSAGE_FROM_CHAT: {
+// 		handleMessageDelete();
+// 		break;
+// 	}
+// 	case REMOVED_FROM_CHAT: {
+// 		hanldeRemoveUserFromChat();
+// 		break;
+// 	}
+// 	case LEAVE_CALL: {
+// 		handleUserLeaveCall();
+// 		break;
+// 	}
+// 	case INCOMING_CALL: {
+// 		handleIncomingCall();
+// 		break;
+// 	}
+// 	case REQUEST_JOIN_CALL: {
+// 		handleRequestJoinCall();
+// 		break;
+// 	}
+// 	case INCOMING_CALL_ACCEPTED: {
+// 		handleIncomingCallAccepted();
+// 		break;
+// 	}
+
+// 	case INCOMING_CALL_REJECTED: {
+// 		handleIncomingCallRejected();
+// 		break;
+// 	}
+// 	case INCOMING_CALL_JOIN_REQUEST: {
+// 		handleIncomingCallJoinRequest();
+// 		break;
+// 	}
+// 	case INCOMING_CALL_JOIN_ACCEPTED: {
+// 		handleIncomingCallJoinAccepted();
+// 		break;
+// 	}
+// 	case REJECT_INCOMING_CALL_JOIN_REQUEST: {
+// 		handleRejectIncomingCallJoinRequest();
+// 		break;
+// 	}
+
+// 	// media soup events
+// 	case "rtpCapabilities": {
+// 		handleRtpCapabilities();
+// 		break;
+// 	}
+// 	case "producer_transport_created": {
+// 		handleProducerTransportCreated();
+// 		break;
+// 	}
+// 	case "producer_connected": {
+// 		handeProducerConnected();
+// 		break;
+// 	}
+// 	case "produced_media": {
+// 		handleProducedMedia();
+// 		break;
+// 	}
+// 	case "consumer_transport_created": {
+// 		handleConsumerTransportCreated();
+// 		break;
+// 	}
+// 	case "consumer_transport_connected": {
+// 		handleConsumerTransportConnected();
+// 		break;
+// 	}
+// 	case "subscribed": {
+// 		handleSubscribed();
+// 		break;
+// 	}
+// 	case "resumed": {
+// 		handleResumed();
+// 		break;
+// 	}
+// 	case "error_on_media": {
+// 		handleErrorOnMedia();
+// 		break;
+// 	}
+// }
--- apps/web/hooks/useHandleSocketEvents.ts
+++ apps/web/hooks/useHandleSocketEvents.ts
@@ -1,23 +1,7 @@
 import { useSocket } from "@/contexts/SocketProvider";
 import { User } from "@/types/user";
 import { useEffect, useState } from "react";
-import {
-	DELETE_MESSAGE_FROM_CHAT,
-	INCOMING_CALL,
-	INCOMING_CALL_ACCEPTED,
-	INCOMING_CALL_JOIN_ACCEPTED,
-	INCOMING_CALL_JOIN_REQUEST,
-	INCOMING_CALL_REJECTED,
-	LEAVE_CALL,
-	RECEIVE_MESSAGE_IN_CHAT,
-	REJECT_INCOMING_CALL_JOIN_REQUEST,
-	REMOVED_FROM_CHAT,
-	REQUEST_CHAT_MESSAGE,
-	REQUEST_JOIN_CALL,
-	USER_MOVEMENT,
-	USERS_NEAR_YOU,
-} from "socket-events";
-import { toast } from "sonner";
+import { USER_MOVEMENT, USERS_NEAR_YOU } from "socket-events";
 
 export const useHandleSocketEvents = () => {
 	const { socket } = useSocket();
@@ -33,43 +17,6 @@ export const useHandleSocketEvents = () => {
 		// WHEN OUR LOCATION IS CHANGES, IT RETURNS TO USER WITHIN THE BOUNDARY
 		setNearUsers(users);
 	};
-	const handleRequestChatMessage = (
-		user: User,
-		chatId: string,
-		message: string
-	) => {
-		toast(`New message from ${user.name}`);
-	};
-	const handleMessageReceive = (
-		sender: User,
-		messageId: string,
-		message: string,
-		chatId: string
-	) => {};
-	const handleDeleteMessage = (messageId: string, userId: string) => {};
-	const handleRemoveUserFromChat = (
-		chatId: string,
-		removedUserId: string
-	) => {};
-	const handleLeaveCall = (userId: string) => {};
-	const handleIncomingCall = (chatId: string, requestedUser: User) => {};
-	const handleRequestJoinCall = (requestedBy: User) => {};
-	const handleIncomingCallAccepted = (chatId: string, acceptedUser: User) => {};
-	const handleIncomingCallJoinRequest = (senderUser: User) => {};
-	const handleIncomingCallJoinAccepted = (chatId: string) => {};
-	const handleRejectIncomingJoinRequest = (rejectedBy: User) => {};
-	const handleRtpCapabilities = (chatId: string, rtpCapabilities: any) => {};
-	const handleProducerTransportCreated = (params: any) => {};
-	const handleProducedMedia = (
-		user: User,
-		chatId: string,
-		producerId: string
-	) => {};
-	const handleConsumerTransportCreated = (params: any, chatId: string) => {};
-	const handleConsumerTransportConnected = (message: any, chatId: string) => {};
-	const handleSubscribed = (params: any, chatId: string) => {};
-	const handleResumed = (message: any) => {};
-	const handleErrorOnMediaSoup = (message: string) => {};
 
 	const handleEventMessage = (message: any) => {
 		const payload = message.payload;
@@ -80,99 +27,6 @@ export const useHandleSocketEvents = () => {
 			}
 			case USERS_NEAR_YOU: {
 				handleNearUsers(payload.aroundUsers);
-				break;
-			}
-			case REQUEST_CHAT_MESSAGE: {
-				handleRequestChatMessage(
-					payload.requestBy,
-					payload.chatId,
-					payload.message
-				);
-				break;
-			}
-			case RECEIVE_MESSAGE_IN_CHAT: {
-				handleMessageReceive(
-					payload.sender,
-					payload.messageId,
-					payload.content,
-					payload.chatId
-				);
-				break;
-			}
-			case DELETE_MESSAGE_FROM_CHAT: {
-				handleDeleteMessage(payload.messageId, payload.by);
-				break;
-			}
-			case REMOVED_FROM_CHAT: {
-				handleRemoveUserFromChat(payload.chatId, payload.removedUserId);
-				break;
-			}
-			case LEAVE_CALL: {
-				handleLeaveCall(payload.leftUserId);
-				break;
-			}
-			case INCOMING_CALL: {
-				//TODO: need to handle call in the call, already existed chat
-				handleIncomingCall(payload.chatId, payload.requestBy);
-				break;
-			}
-			case REQUEST_JOIN_CALL: {
-				handleRequestJoinCall(payload.requestBy);
-				break;
-			}
-			case INCOMING_CALL_ACCEPTED: {
-				handleIncomingCallAccepted(payload.chatId, payload.acceptedBy);
-				break;
-			}
-			case INCOMING_CALL_REJECTED: {
-				// TODO: NOT quite sure
-				// handleIncomingCallRejected(payload.rejectedBy)
-				break;
-			}
-			case INCOMING_CALL_JOIN_REQUEST: {
-				handleIncomingCallJoinRequest(payload.from);
-				break;
-			}
-			case INCOMING_CALL_JOIN_ACCEPTED: {
-				// TODO: NOT SURE as well
-				handleIncomingCallJoinAccepted(payload.chatId);
-				break;
-			}
-			case REJECT_INCOMING_CALL_JOIN_REQUEST: {
-				handleRejectIncomingJoinRequest(payload.rejectedBy);
-				break;
-			}
-			case "rtpCapabilities": {
-				handleRtpCapabilities(payload.chatId, payload.rtpCapabilities);
-				break;
-			}
-			case "producer_transport_created": {
-				handleProducerTransportCreated(payload);
-				break;
-			}
-			// ONE LEFT HERE
-			case "produced_media": {
-				handleProducedMedia(payload.user, payload.chatId, payload.producerId);
-				break;
-			}
-			case "consumer_transport_created": {
-				handleConsumerTransportCreated(payload.params, payload.chatId);
-				break;
-			}
-			case "consumer_transport_connected": {
-				handleConsumerTransportConnected(payload.message, payload.chatId);
-				break;
-			}
-			case "subscribed": {
-				handleSubscribed(payload.params, payload.chatId);
-				break;
-			}
-			case "resumed": {
-				handleResumed(payload.message);
-				break;
-			}
-			case "error_on_media": {
-				handleErrorOnMediaSoup(payload.message);
 				break;
 			}
 		}
