71872d5b550dbacdba52225f4efec8f2f52a7bfc
Bibek Tamang <bibek7here@gmail.com>
2025-06-09 19:28:39 +0545

    generate jwt token

--- apps/web/app/api/auth/[...nextauth]/auth.ts
+++ apps/web/app/api/auth/[...nextauth]/auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { prismaClient } from "db";
+import jwt from "jsonwebtoken";
 
 import type { NextAuthConfig, NextAuthResult } from "next-auth";
 
@@ -24,13 +26,29 @@ const result = NextAuth({
 	...config,
 	session: {
 		strategy: "jwt",
+		maxAge: 84600 * 7,
 	},
 	callbacks: {
 		async signIn({ account, profile, user }) {
 			if (account?.provider === "google") {
 				if (!profile?.email) return false;
 				try {
-					return true;
+					const createdUser = await prismaClient.user.findUnique({
+						where: {
+							email: profile.email,
+						},
+					});
+					user.id = createdUser?.id;
+					if (!createdUser) {
+						const userCreated = await prismaClient.user.create({
+							data: {
+								email: profile.email,
+								image: profile.picture,
+								name: profile.name ?? `Guest-${profile.email.split("@")[0]}`,
+							},
+						});
+						user.id = userCreated.id;
+					}
 				} catch (error) {
 					throw Error("Not Google Provider");
 				}
@@ -41,13 +59,28 @@ const result = NextAuth({
 			if (account && profile) {
 				token.id = user.id;
 				token.image = profile.picture;
+				token.name = profile.name ?? `Guest-${profile.email?.split("@")[0]}`;
+				const signToken = jwt.sign(
+					{
+						id: user.id,
+						image: profile.picture,
+						name: profile.name ?? `Guest-${profile.email?.split("@")[0]}`,
+					},
+					process.env.TOKEN_SECRET!,
+					{
+						expiresIn: "7d",
+					}
+				);
+				token.accessToken = signToken;
 			}
 			return token;
 		},
 		async session({ session, token }) {
 			if (session.user) {
+				session.accessToken = token.accessToken as string;
 				session.user.id = token.id as string;
 				session.user.image = token.picture as string;
+				session.user.name = token.name as string;
 			}
 			return session;
 		},
